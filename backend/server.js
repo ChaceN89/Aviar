47a21f5a478720cf67881514fa048c547063964d
Chace Nielson <chacen@shaw.ca>
2022-02-27 21:59:55 -0700

    updated server.js adn errormiddleware.js - just basic things added and nothing specific, system will run server but doenst do anything yet

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,15 @@
 //entry point to server
-console.log("Aviar Server.js running")
+console.log("Aviar server.js running");
+
+
+const connectDB = require('./config/db'); // require the db file to acces mongodb
+
+const express = require('express'); // get express
+const dotenv = require('dotenv').config(); // use dotenv to load environment variables from .env file.
+const{errorHandler} = require('./middleware/errorMiddleware');
+const port = process.env.PORT || 8000; // or 8000 if the enviroment variable in .env isn't found
+const app = express();
+
+
+
+console.log("End of server.js code");
