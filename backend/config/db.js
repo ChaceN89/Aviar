9c7b3a3d91fbe8d14ed784ad209a22a237afaee2
Chace Nielson <chacen@shaw.ca>
2022-03-11 08:27:53 -0700

    db.js completed and added to server.js as well as add functionality to server.js

---
+++ backend/config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose'); // should be installed in package.JSon
+
+const colors= require('colors'); // can use this anywhere since its in the servee,.js file
+const dotenv = require('dotenv').config(); // need for using process.env.MONGO_URI
+// not sure why this isn't just imported from server.js but this currently works
+
+const connectDB = async() => {
+    console.log("Connecting to MongoAtlas database with mongoose".grey.underline);
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI); // connect to databse using connection stirng in .env
+        console.log(`MongoDb connected: ${conn.connection.host}`.cyan.underline); // last part if from colors package to make it easier
+    
+    } catch (error) {
+            console.log("---------------------ABOUT TO GET ERRORS MESSAGE-------------");
+            console.log(error);
+            process.exit(1);
+            console.log("---------------------shouldn't get here-------------");
+    }
+} //end fucntiion
+
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,23 @@ console.log("Aviar server.js running");
 
 
 const connectDB = require('./config/db'); // require the db file to acces mongodb
+connectDB(); //set up a donnection to the database 
 
+//set up values to sue in server
 const express = require('express'); // get express
 const dotenv = require('dotenv').config(); // use dotenv to load environment variables from .env file.
-const{errorHandler} = require('./middleware/errorMiddleware');
-const port = process.env.PORT || 8000; // or 8000 if the enviroment variable in .env isn't found
+const{errorHandler} = require('./middleware/errorMiddleware'); // my errorhandler system that replaces the defualt
+
+const port = process.env.PORT || 8000; // or 8000 if the enviroment variable in .env isn't available
 const app = express();
 
+//routes/endpoints
 
+
+
+app.use(errorHandler); // override default error handlers
+
+//listen() function is used to bind and listen the connections on the specified host and port.
+app.listen(port, ()=> console.log("server started on http://localhost:" + port));
 
 console.log("End of server.js code");
