e0e2833358a222f08e3ed31bbeabc2c40f57822e
ivanezekielsuyat <81483920+ivanezekielsuyat@users.noreply.github.com>
2022-03-27 16:34:49 -0600

    Update AddToCollection.jsx

--- frontend/src/components/AddToCollection.jsx
+++ frontend/src/components/AddToCollection.jsx
@@ -1,17 +1,61 @@
 import React from 'react'
 
+//### myCol
+import { useState, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import Spinner from '../components/Spinner'
+import {
+  AccordionWithHeader,
+  AccordionNode,
+  AccordionHeader,
+  AccordionPanel
+} from 'react-accordion-with-header'
+import Modal from 'react-modal'
+import { FaPen } from 'react-icons/fa'
+import {
+  deleteCollection,
+  getCollections,
+  reset,
+  updateCollectionName
+} from '../features/collections/collectionSlice'
+
+//### website
+// import ReactDOM from "react-dom";
+
+// const App = () => {
+//     return(<div>
+//         Hello
+//     </div>);
+// };
+
+// ReactDOM.render(<App />, document.querySelector("#root"));
+
+
 
 function AddToCollection({ postId, user, post }) {
     const collections = true; // test for 
             //this will be a determination of failure or success for getting users colelctionss
   
-        //functionality to get collections 
-
+        //functionality to get collections
+    
     return (
     <>
     {collections ?(
         <>
-         
+
+        <br></br> 
+        <b>Add post to an existing collection or create a new collection: </b><br></br>
+        
+        <select>
+            {collections.map((i) =>
+            <>
+                <option value={i._id}>Hello</option>
+            </>)}
+        </select>
+        
+      
+
         <div>add to Collection 1</div>
         <div>add to Collection 2</div>
 
