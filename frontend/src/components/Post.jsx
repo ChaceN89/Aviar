48e479d55b254a45c655f40df1c4f2fcaa649513
jacob <jacobn0223@outlook.com>
2022-03-26 20:42:15 -0600

    Added context for post component

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import { Avatar } from '@material-ui/core';
+import React from 'react'
+import { Avatar } from '@material-ui/core'
 import { useState, useEffect } from 'react'
-import { toast } from 'react-toastify';
+import { toast } from 'react-toastify'
 
 import Spinner from '../components/Spinner'
 import { useSelector, useDispatch } from 'react-redux'
 import { useNavigate, Link } from 'react-router-dom'
 
-import {
-  addComment,
-  reset
-} from '../features/posts/postSlice'
+import { addComment, reset } from '../features/posts/postSlice'
 
-function Post({ postId, user, caption, theme, medium, imageURL, comments }) {
+function Post ({
+  postId,
+  user,
+  caption,
+  theme,
+  medium,
+  imageURL,
+  comments,
+  context
+}) {
   const [newComment, setNewComment] = useState()
 
   // const { user } = useSelector(state => state.auth) // get user
@@ -20,7 +26,8 @@ function Post({ postId, user, caption, theme, medium, imageURL, comments }) {
   const navigate = useNavigate()
   const dispatch = useDispatch()
 
-  const { // get post status inforamtion 
+  const {
+    // get post status inforamtion
     isError,
     commentAdded,
     isSuccess,
@@ -29,97 +36,106 @@ function Post({ postId, user, caption, theme, medium, imageURL, comments }) {
   } = useSelector(state => state.post)
 
   useEffect(() => {
-
-    if (commentAdded) { // if the comemnt is added
-      window.location.reload(); // refresh the page
-
+    if (commentAdded) {
+      // if the comemnt is added
+      window.location.reload() // refresh the page
     }
 
-    if (isError && isSuccess) { // error with comment and there is a post to comment on 
-      toast.error('Couldn\'t add Comment')
+    if (isError && isSuccess) {
+      // error with comment and there is a post to comment on
+      toast.error("Couldn't add Comment")
     }
 
-    return () => { // might not be able to call this reset
-      // dispatch(reset()) // refresh will take can of reset in 
+    return () => {
+      // might not be able to call this reset
+      // dispatch(reset()) // refresh will take can of reset in
       //PostPage
     }
-
   }, [isError, dispatch, commentAdded])
-
 
   const onChange = e => {
     setNewComment(e.target.value)
-  };
-
-  const onSubmit = async e => {
-    e.preventDefault();
-
-    //need post data
-    const formData = new FormData();  // set form data
-    formData.append('postId', postId);
-    formData.append('newComment', newComment);
-
-    dispatch(addComment({
-      postId: postId,
-      newComment: newComment
-    }))
-
   }
 
-  // does this on the page calling it 
-  // if (isLoading) { // this might cause an error if post doens't exisit not sure why but its hjsut for show so its not nessaesary 
+  const onSubmit = async e => {
+    e.preventDefault()
+
+    //need post data
+    const formData = new FormData() // set form data
+    formData.append('postId', postId)
+    formData.append('newComment', newComment)
+
+    dispatch(
+      addComment({
+        postId: postId,
+        newComment: newComment
+      })
+    )
+  }
+
+  // does this on the page calling it
+  // if (isLoading) { // this might cause an error if post doens't exisit not sure why but its hjsut for show so its not nessaesary
   //   return <Spinner />
   // }
 
   return (
-    <div className="post">
+    <div className='post'>
       {/* image  this works on dashboard with hard coded inforamtion*/}
-      <Link to={'/post/' + postId}>
-        <img className="post__piece"
+      {context === 'page' ? (
+        <img
+          className='post__piece'
           src={process.env.PUBLIC_URL + '/uploads/' + imageURL}
           alt={imageURL}
-          width="768" height="520"
+          width='768'
+          height='520'
         />
-      </Link>
+      ) : (
+        <Link to={'/post/' + postId}>
+          <img
+            className='post__piece'
+            src={process.env.PUBLIC_URL + '/uploads/' + imageURL}
+            alt={imageURL}
+            width='768'
+            height='520'
+          />
+        </Link>
+      )}
 
       {/* avatar + artist */}
-      <div className="post__bottom">
+      <div className='post__bottom'>
         <Avatar
-          className="post__avatar"
-          src="/static/images/avatar/1.jpg"
-          alt=""
+          className='post__avatar'
+          src='/static/images/avatar/1.jpg'
+          alt=''
         />
 
         {/* caption */}
-        <h4 className='post__text'><strong>{theme}</strong> - {medium}</h4>
+        <h4 className='post__text'>
+          <strong>{theme}</strong> - {medium}
+        </h4>
       </div>
-      <h3 className='post__text'><strong>{caption}</strong> </h3>
-      <div className="post__comments">
-        {
-          comments.map((comment) => (
-            <p>
-              -  {comment}
-            </p>
-          ))
-        }
+      <h3 className='post__text'>
+        <strong>{caption}</strong>{' '}
+      </h3>
+      <div className='post__comments'>
+        {comments.map(comment => (
+          <p>- {comment}</p>
+        ))}
       </div>
       {user && ( // if user then comment is available
-        <form className="post__commentBox " onSubmit={onSubmit}>
-          <input className="post__input"
-            type="text"
-            placeholder="Comment your thoughts"
+        <form className='post__commentBox ' onSubmit={onSubmit}>
+          <input
+            className='post__input'
+            type='text'
+            placeholder='Comment your thoughts'
             value={newComment}
             onChange={onChange}
           />
-          <button
-            className='post__button'
-            type="submit"
-          >
+          <button className='post__button' type='submit'>
             Comment
           </button>
         </form>
       )}
-
     </div>
   )
 }
